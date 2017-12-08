4bef9c5beba66813071786d62e3c06d31d932403
Shawn <shawn@frozen.ca>
2017-12-07 18:46:21 -0600

    Initial version.

---
+++ ViewController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UIKit;
+
+namespace TestFreeType
+{
+    public partial class ViewController : UIViewController
+    {
+        [DllImport("__Internal", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int FT_Init_FreeType(out IntPtr library);
+
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            IntPtr lib;
+            int result = FT_Init_FreeType(out lib);
+        }
+    }
+}
